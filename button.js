20fed969d4a466b160a29236001569d8661aec63
leesky509 <leesky509@naver.com>
2023-01-16 19:24:17 +0900

    second commit

--- button.js
+++
@@ -1,9 +0,0 @@
-function buttonHandler() {
-  var button = document.querySelector(button);
-  var menus = ["Hello World", "HTML", "CSS", "Java script"];
-  var i = 0;
-  while (i < menus.length) {
-    document.write("<button>" + menus[i] + "</button>");
-    i = i + 1;
-  }
-}
