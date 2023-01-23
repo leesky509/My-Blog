a10f15403d6b49a9d46dfef7c9289494abedb492
leesky509 <leesky509@naver.com>
2023-01-24 01:28:44 +0900

    while문 > for문 수정, 배열-메뉴/링크 분리

--- Script.js
+++ Script.js
@@ -1,11 +1,13 @@
 function menusHandler() {
-  var menus = ["Hello World", "HTML", "CSS", "JavaScript", "기술 블로그"];
-  var i = 0;
-  while (i < menus.length) {
-    document.write(
-    '<li><a href="' + menus[i] + '.html" class="menusColor">'
-     + menus[i] + "</a></li>"
-    );
-    i = i + 1;
-  }
-}
+  var menus = ["Main", "HTML", "CSS", "JavaScript", "기술 블로그"];
+  var links = ["index", "html", "css", "javascript", "tech-blog"];
+    for (var i = 0; i < menus.length; i++) {
+        document.write(
+          '<li><a href="' +
+            links[i] +
+            '.html" class="menusColor">' +
+            menus[i] +
+            "</a></li>"
+        );
+      }
+    }
