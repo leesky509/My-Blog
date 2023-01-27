3abde2902471f54bf763c2e39c85630d78711071
leesky509 <leesky509@naver.com>
2023-01-27 20:04:55 +0900

    js 100제 폴딩기능 추가

--- menus/script.js
+++ menus/script.js
@@ -1,4 +1,4 @@
-function menusHandler() {
+/* function menusHandler() {
   var menus = ["Main", "HTML", "CSS", "JavaScript", "기술 블로그"];
   var links = ["blog-main", "html", "css", "javascript", "tech-blog"];
   for (var i = 0; i < menus.length; i++) {
@@ -9,5 +9,15 @@ function menusHandler() {
         menus[i] +
         "</a></li>"
     );
+  }
+}
+*/
+
+function GetShow() {
+  var con = document.getElementById("divcon");
+  if (con.style.display == "none") {
+    con.style.display = "block";
+  } else {
+    con.style.display = "none";
   }
 }
